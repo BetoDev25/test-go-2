f2f1e3375b304092284ccd3433ce47cb9422fe22
NorMeni <BetoDev25@users.noreply.github.com>
2026-02-22 19:08:24 -0500

    fourth commit

---
+++ handler_validate_chirp.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type response struct {
+		Valid bool   `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	respondWithJSON(w, 200, response{
+		Valid: true,
+	})
+}
--- main.go
+++ main.go
@@ -4,10 +4,33 @@ import (
 	"log"
 	"net/http"
 	"sync/atomic"
+	"encoding/json"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
 }
 
 func main() {
@@ -27,6 +50,7 @@ func main() {
 	mux.Handle("GET /api/healthz", http.HandlerFunc(handlerReadiness))
 	mux.Handle("GET /admin/metrics", http.HandlerFunc(apiCfg.handlerMetrics))
 	mux.Handle("POST /admin/reset",   http.HandlerFunc(apiCfg.handlerReset))
+	mux.Handle("POST /api/validate_chirp", http.HandlerFunc(apiCfg.handlerValidateChirp))
 
 	log.Fatal(server.ListenAndServe())
 }
